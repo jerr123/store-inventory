9d44581bc26c80d8bbca070613f57c49284ede54
杨再坤 <2273716951@qq.com>
2020-02-22 20:47:46 +0800

    fix: 调整接口频率限制

--- routes/api.php
+++ routes/api.php
@@ -35,6 +35,14 @@ $api->version('v1', [
         $api->delete('authorizations/current', 'AuthorizationsController@destroy')
             ->name('api.authorizations.destroy');
 
+
+    });
+
+    $api->group([
+        'middleware' => 'api.throttle',
+        'limit' => config('api.rate_limits.access.limit'),
+        'expires' => config('api.rate_limits.access.expires'),
+    ], function ($api) {
         // 需要 token 的的接口
         $api->group(['middleware' => 'api.auth'], function($api) {
             // 当前登录用户信息
